be77907e86058f85df21cfaf9717eac26a5614ba
NWTC--BEBT-NBTC- <119737658+anonim-01@users.noreply.github.com>
2025-11-16 09:36:54 +0300

    feat: Next.js için temel sayfa yapısını ekle

---
+++ next/pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function HomePage() {
+  return (
+    <div>
+      <h1>Merhaba, Next.js!</h1>
+      <p>Bu, Netlify üzerinde çalışan statik bir Next.js sayfasıdır.</p>
+      <p>Projenin ana PHP uygulaması Cloudflare üzerinde çalışmaya devam ediyor.</p>
+    </div>
+  );
+}
